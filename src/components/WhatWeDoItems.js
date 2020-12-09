f319c3b7ded35ad4bcb3d5f4a8e7f2892ab8b954
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-09 15:54:00 +0000

    Added alternating Left - Right content on What we do page

--- src/components/WhatWeDoItems.js
+++ src/components/WhatWeDoItems.js
@@ -2,17 +2,43 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
+var id = 0;
+
+const ConsoleLog = ({ children }) => {
+  console.log(children);
+  return false;
+};
+
+function getId() {
+  console.log(id)
+  return id++;
+}
+
 const WhatWeDoGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
     {gridItems.map((item) => (
       <div key={item.text} className="columns is-mobile is-multiline is-vcentered">
-          <div className="column">
-            <PreviewCompatibleImage imageInfo={item} />
-          </div>
-          <div className="column">
-            <h1>{item.title}</h1>
-            <p>{item.text}</p>
-          </div>
+        {getId() % 2 == 0 ? (
+          <>
+            <div className="column">
+              <PreviewCompatibleImage imageInfo={item} />
+            </div>
+            <div className="column">
+              <h1>{item.title}</h1>
+              <p>{item.text}</p>
+            </div>
+          </>
+          ) : (
+            <>
+            <div className="column">
+              <h1>{item.title}</h1>
+              <p>{item.text}</p>
+            </div>
+            <div className="column">
+              <PreviewCompatibleImage imageInfo={item} />
+            </div>
+          </>
+          )}
       </div>
     ))}
   </div>
