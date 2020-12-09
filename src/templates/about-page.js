28b653afa6e2674d44b21ed21865074f892365dd
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-09 18:06:54 +0000

    Cleared contents of about page

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -13,10 +13,12 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+             {/* <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
               <PageContent className="content" content={content} />
+              */}
+              <h1 className="has-text-centered"> What to put here? </h1>
             </div>
           </div>
         </div>
