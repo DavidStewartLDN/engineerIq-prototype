cf5a1f8f7138ecf6abf2daef31786bf9b4e22ca8
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-09 16:27:20 +0000

    Updated website title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Engineer iQ',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Enigneer iQ Website',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
