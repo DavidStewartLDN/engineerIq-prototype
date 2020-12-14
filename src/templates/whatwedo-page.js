138d96405057e57f92c9d44b614bb86c9b5e7799
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-14 11:00:35 +0000

    Removed title from on top of image WhatWeDo page

--- src/templates/whatwedo-page.js
+++ src/templates/whatwedo-page.js
@@ -18,17 +18,6 @@ export const WhatWeDoTemplate = ({
         backgroundImage: `url('/img/tank-farm-1.jpg')`,
       }}
     >
-    <h1
-      className="has-text-weight-bold is-size-1"
-      style={{
-        boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-        backgroundColor: '#f40',
-        color: 'white',
-        padding: '1rem',
-      }}
-    >
-    What we do
-    </h1>
   </div>
     <section className="section section--gradient">
       <div className="container">
