9daa139a4acb0cdbf035a83a10311c5f4ca8d5bd
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-15 00:05:02 +0000

    Corrected WhatWeDo grid order

--- src/components/WhatWeDoItems.js
+++ src/components/WhatWeDoItems.js
@@ -1,24 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
-var id = 0;
-
-const ConsoleLog = ({ children }) => {
-  console.log(children);
-  return false;
-};
-
-function getId() {
-  console.log(id)
-  return id++;
-}
 
 const WhatWeDoGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
-    {gridItems.map((item) => (
+    {gridItems.map((item, index) => (
       <div key={item.text} className="columns is-mobile is-multiline is-vcentered pt-6">
-        {getId() % 2 == 0 ? (
+        {index % 2 === 0 ? (
           <>
             <div className="column">
               <PreviewCompatibleImage imageInfo={item} />
