4264a1cd1674bdbaf05d01ec143f87ca6b113928
DavidStewartLDN <davidstewart.work@gmail.com>
2020-12-29 19:28:20 +0000

    Corrected capitalisation for React on playsInLine

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const AboutGrid = ({ gridItems }) => (
         </div>
         <div className="column is-6">
           <div className="is-desktop is-vcentered">
-            <video className="is-desktop is-vcentered" autoPlay muted loop playsinline>
+            <video className="is-desktop is-vcentered" autoPlay muted loop playsInLine>
               <source src={EngIqVideo} type="video/mp4"/>
             </video>
           </div>
